d4093b7d0b8fc5e0ac9faf14fc406d44a8fd8ff0
Kim Ye Eun <101261625+yeeun0702@users.noreply.github.com>
2025-05-29 15:50:35 +0900

    Update 중앙값 구하기.java

--- Java/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ Java/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] array) {
-        int answer = 0;
-        return answer;
+        Arrays.sort(array);
+        int n = array.length/2;
+        return array[n];
     }
 }
