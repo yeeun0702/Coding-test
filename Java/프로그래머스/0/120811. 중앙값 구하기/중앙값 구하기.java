ec30253ab5946474d15dd64f6d9f22a8e7c22e3e
Kim Ye Eun <101261625+yeeun0702@users.noreply.github.com>
2025-05-29 15:48:30 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.50 ms, Memory: 72.9 MB -BaekjoonHub

--- Java/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ Java/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,9 +1,6 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] array) {
-        Arrays.sort(array);
-        int n = array.length/2;
-        return array[n];
+        int answer = 0;
+        return answer;
     }
 }
