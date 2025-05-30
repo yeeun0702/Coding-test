238208624b6d4e2fe6729218861bba9a60110bed
Kim Ye Eun <101261625+yeeun0702@users.noreply.github.com>
2025-05-30 21:20:06 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 96.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        
+        // 순서를 뒤집을 새로운 배열
+        int[] answer = new int[num_list.length];
+        
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = num_list[num_list.length - 1 - i];
+        }
+
+        return answer;
+    }
+}
